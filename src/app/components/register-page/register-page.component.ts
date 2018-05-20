2f8db781a26a77057b5bde1a5dd8d903a2078a4e
kyriuch <kyriuch@gmail.com>
2018-05-21 01:05:49 +0200

    Almost finished up work on register form

--- src/app/components/register-page/register-page.component.ts
+++ src/app/components/register-page/register-page.component.ts
@@ -1,11 +1,30 @@
 import { Component, OnInit } from '@angular/core';
 import { BreakpointObserver, BreakpointState } from '@angular/cdk/layout';
 import { FormBuilder, FormGroup, Validators, FormControl } from '@angular/forms';
+import { matchOtherValidator } from '../../validators/password-validator';
+import { trigger, style, state, transition, animate, keyframes, query, stagger } from '@angular/animations';
+
 
 @Component({
   selector: 'app-register-page',
   templateUrl: './register-page.component.html',
-  styleUrls: ['./register-page.component.scss']
+  styleUrls: ['./register-page.component.scss'],
+  animations: [
+    trigger('shake', [
+      transition('* => *', [
+        animate(150, keyframes([
+          style({ 'margin-left': '10px', 'margin-right': '0px' }),
+          style({ 'margin-left': '0px', 'margin-right': '10px' }),
+          style({ 'margin-left': '10px', 'margin-right': '0px' }),
+          style({ 'margin-left': '0px', 'margin-right': '20px' }),
+          style({ 'margin-left': '10px', 'margin-right': '0px' }),
+          style({ 'margin-left': '0px', 'margin-right': '10px' }),
+          style({ 'margin-left': '10px', 'margin-right': '0px' }),
+          style({ 'margin-left': '0px', 'margin-right': '0px' }),
+        ]))
+      ])
+    ])
+  ]
 })
 export class RegisterPageComponent implements OnInit {
 
@@ -13,20 +32,95 @@ export class RegisterPageComponent implements OnInit {
 
   registrationForm: FormGroup;
 
-  constructor(private breakpointObserver: BreakpointObserver) { }
+  emailState: boolean;
+  passwordState: boolean;
+  repeatPasswordState: boolean;
+  steamIdState: boolean;
+
+  constructor(private breakpointObserver: BreakpointObserver, private fb: FormBuilder) {
+    this.createForm();
+  }
 
   ngOnInit() {
-    this.breakpointObserver.observe('(max-width: 768px)').subscribe((state: BreakpointState) => {
-      this.smallScreen = state.matches;
+    this.breakpointObserver.observe('(max-width: 768px)').subscribe((breakpointState: BreakpointState) => {
+      this.smallScreen = breakpointState.matches;
     });
+  }
 
-    this.registrationForm = new FormGroup({
-      userData: new FormGroup({
-        email: new FormControl(),
-        password: new FormControl(),
-        repeatPassword: new FormControl(),
-        steamId: new FormControl()
-      })
+  createForm() {
+    this.registrationForm = this.fb.group({
+      email: ['', [Validators.required, Validators.email]],
+      password: ['', [Validators.required, Validators.minLength(6), Validators.maxLength(32)]],
+      repeatPassword: ['', [Validators.compose(
+        [Validators.required, Validators.minLength(6), Validators.maxLength(32), matchOtherValidator('password')]
+      )]],
+      steamId: ['', [Validators.required, Validators.pattern('^STEAM_[0-5]:[01]:[0-9]+$')]]
     });
+  }
+
+  emailError() {
+    const control = this.registrationForm.get('email');
+
+    if (control.hasError('required')) {
+      return 'Uzupełnij adres email';
+    } else if (control.hasError('email')) {
+      return 'Adres email nieprawidłowy';
+    }
+  }
+
+  passwordError() {
+    const control = this.registrationForm.get('password');
+
+    if (control.hasError('required')) {
+      return 'Uzupełnij hasło';
+    } else if (control.hasError('minlength')) {
+      return 'Minimalna długość hasła to 6';
+    } else if (control.hasError('maxlength')) {
+      return 'Maksymalna długość hasła to 32';
+    }
+  }
+
+  repeatPasswordError() {
+    const control = this.registrationForm.get('repeatPassword');
+
+    if (control.hasError('required')) {
+      return 'Uzupełnij hasło';
+    } else if (control.hasError('minlength')) {
+      return 'Minimalna długość hasła to 6';
+    } else if (control.hasError('maxlength')) {
+      return 'Maksymalna długość hasła to 32';
+    } else if (control.hasError('matchOther')) {
+      return 'Hasła nie pasują do siebie';
+    }
+  }
+
+  steamIdError() {
+    const control = this.registrationForm.get('steamId');
+
+    if (control.hasError('required')) {
+      return 'Uzupełnij Steam';
+    } else if (control.hasError('pattern')) {
+      return 'SteamID nieprawidłowy';
+    }
+  }
+
+  onSubmit() {
+    if (!this.registrationForm.valid) {
+      if (!this.registrationForm.get('email').valid) {
+        this.emailState = !this.emailState;
+      }
+
+      if (!this.registrationForm.get('password').valid) {
+        this.passwordState = !this.passwordState;
+      }
+
+      if (!this.registrationForm.get('repeatPassword').valid) {
+        this.repeatPasswordState = !this.repeatPasswordState;
+      }
+
+      if (!this.registrationForm.get('steamId').valid) {
+        this.steamIdState = !this.steamIdState;
+      }
+    }
   }
 }
