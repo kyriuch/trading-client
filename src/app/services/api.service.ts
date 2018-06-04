067956f9723df578c86ed7d132293657fd05ead0
kyriuch <kyriuch@gmail.com>
2018-06-04 17:16:28 +0200

    Jwt finally working

--- src/app/services/api.service.ts
+++ src/app/services/api.service.ts
@@ -8,40 +8,34 @@ import { ApiPostModel, ApiAuthorizedGetModel } from '../models/api-models';
 })
 export class ApiService {
 
-
-  httpHeaders: HttpHeaders;
   apiUrl: string;
 
   constructor(private httpClient: HttpClient) {
-    this.httpHeaders = new HttpHeaders();
-    this.httpHeaders.append('Content-Type', 'application/json');
-    this.httpHeaders.append('Accept', 'application/json');
     this.apiUrl = 'https://localhost:44391/api/';
   }
 
   post<T>(apiModel: ApiPostModel): Observable<T> {
-    this.detachToken();
-
     return this.httpClient.post<T>(this.apiUrl + apiModel.apiEndpoint,
       apiModel.requestBody,
       {
-        headers: this.httpHeaders
+        headers: this.generateHeaders()
       });
   }
 
   get(apiModel: ApiAuthorizedGetModel): Observable<any> {
-    this.attachToken(apiModel.token);
+    let httpHeaders = this.generateHeaders();
+    httpHeaders = httpHeaders.append('Authorization', `Bearer ${apiModel.token}`);
+    console.log(httpHeaders.getAll('Authorization'));
 
     return this.httpClient.get(this.apiUrl + apiModel.apiEndpoint, {
-      headers: this.httpHeaders
+      headers: httpHeaders
     });
   }
 
-  private attachToken(token: string) {
-    this.httpHeaders.append('Authorization', `Bearer ${token}`);
-  }
-
-  private detachToken() {
-    this.httpHeaders.delete('Authorization');
+  private generateHeaders(): HttpHeaders {
+    let httpHeaders = new HttpHeaders();
+    httpHeaders = httpHeaders.append('Content-Type', 'application/json');
+    httpHeaders = httpHeaders.append('Accept', 'application/json');
+    return httpHeaders;
   }
 }
