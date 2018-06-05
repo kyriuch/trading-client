6d1fe13b1d1f6fda748fe9b4b1979ad75946b264
kyriuch <kyriuch@gmail.com>
2018-06-05 11:32:10 +0200

    forgot to attach this to a commit :(

--- src/app/services/user-guard.service.ts
+++ src/app/services/user-guard.service.ts
@@ -1,9 +1,14 @@
 import { Injectable } from '@angular/core';
+import { CanActivate } from '@angular/router';
 
 @Injectable({
   providedIn: 'root'
 })
-export class UserGuardService {
+export class UserGuardService implements CanActivate {
+
+  canActivate() {
+
+  }
 
   constructor() { }
 }
